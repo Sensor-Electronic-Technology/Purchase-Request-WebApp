c774ae4a2c568fef7a7b898251cf7edddc560980
AElmendo <aelmendorf234@gmail.com>
2024-09-25 17:01:50 -0400

    m

--- Webapp/Program.cs
+++ Webapp/Program.cs
@@ -7,9 +7,7 @@ using Webapp.Components;
 using Webapp.Services.Authentication;
 using Infrastructure;
 using Infrastructure.Services;
-
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
@@ -41,50 +39,5 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
-app.Urls.Add("http://0.0.0.0:8080");
+//app.Urls.Add("http://0.0.0.0:8080");
 app.Run();
-
-
-/*builder.Services.AddAuthentication().AddCookie("SetiScheme", options => {
-    options.Cookie.Name = "SetiAuth";
-    options.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.Always;
-    options.Cookie.HttpOnly = true;
-    options.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Lax;
-    options.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.Always;
-    options.Cookie.IsEssential = true;
-    options.LoginPath = "/login";
-    options.LogoutPath = "/logout";
-    options.AccessDeniedPath = "/accessdenied";
-    options.ReturnUrlParameter = "returnUrl";
-});*/
-/*builder.Services.AddAuthenticationCore().AddCookiePolicy(options => {
-    options.ConsentCookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.Always;
-    options.ConsentCookie.Name = "SetiAuth";
-    options.ConsentCookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.Always;
-    options.ConsentCookie.HttpOnly = true;
-    options.ConsentCookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Strict;
-    options.ConsentCookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.Always;
-    options.ConsentCookie.IsEssential = true;
-});*/
-/*builder.Services.AddAuthenticationCore(options => {
-    options.AddScheme("")
-});
-options.AddPolicy("EditUser", policy =>
-    policy.RequireAssertion(context =>
-    {
-        if (context.Resource is RouteData rd)
-        {
-            var routeValue = rd.RouteValues.TryGetValue("id", out var value);
-            var id = Convert.ToString(value,
-                System.Globalization.CultureInfo.InvariantCulture) ?? string.Empty;
-
-            if (!string.IsNullOrEmpty(id))
-            {
-                return id.StartsWith("EMP", StringComparison.InvariantCulture);
-            }
-        }
-
-        return false;
-    })
-);*/
