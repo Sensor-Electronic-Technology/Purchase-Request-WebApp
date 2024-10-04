0d6d928989e34ef0511ff07c47449ccdb93748ee
AElmendo <aelmendorf234@gmail.com>
2024-10-04 16:35:48 -0400

    RequestAlert styles

--- Webapp/Data/RequestAlert.cs
+++ Webapp/Data/RequestAlert.cs
@@ -3,7 +3,7 @@
 namespace Webapp.Data;
 
 public class RequestAlert {
-    public string? Text { get; set; }
+    public string? Message { get; set; }
     public string? Item { get; set; }
     public bool Okay { get; set; }
     public AlertStyle Style { get; set; }
