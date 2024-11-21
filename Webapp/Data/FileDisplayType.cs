5eb3de2b6ff0a4f0a481bae9069fe0f1b96e7b6e
AElmendo <aelmendorf234@gmail.com>
2024-11-21 13:48:13 -0500

    UI fine tuning

--- Webapp/Data/FileDisplayType.cs
+++ Webapp/Data/FileDisplayType.cs
@@ -7,6 +7,18 @@ public class FileDisplayType:SmartEnum<FileDisplayType,string> {
     public static readonly FileDisplayType DOC=new(nameof(DOC),"Document",[".doc",".docx"]);
     public static readonly FileDisplayType OTHER=new(nameof(OTHER),"Other",[".xls",".xlsx"]);
     public IEnumerable<string> Extensions;
+    public string Icon=>Value switch {
+        "PDF"=>"picture_as_pdf",
+        "Image"=>"image",
+        "Document"=>"article",
+        "Other"=>"table",
+        _=>"fa-file"
+    };
+    
+    public static FileDisplayType FromExtension(string ext) {
+        var fileDisplayType= List.FirstOrDefault(x=>x.Extensions.Contains(ext));
+        return fileDisplayType ?? OTHER;
+    }
 
     private FileDisplayType(string name, string value, params IEnumerable<string> ext) : base(name, value) {
         this.Extensions = ext;
