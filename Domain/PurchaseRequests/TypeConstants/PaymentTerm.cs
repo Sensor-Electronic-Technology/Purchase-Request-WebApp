8b6633da3e72fe65610712676214189ac7aafc12
AElmendo <aelmendorf234@gmail.com>
2026-05-18 12:28:38 -0400

    Add FCA shipping term

--- Domain/PurchaseRequests/TypeConstants/PaymentTerm.cs
+++ Domain/PurchaseRequests/TypeConstants/PaymentTerm.cs
@@ -4,6 +4,7 @@ namespace Domain.PurchaseRequests.TypeConstants;
 
 public class PaymentTerm:SmartEnum<PaymentTerm, string> {
     public static readonly PaymentTerm FOB = new PaymentTerm(nameof(FOB), "FOB");
+    public static readonly PaymentTerm FCA = new PaymentTerm(nameof(FCA), "FCA");
     public static readonly PaymentTerm CIF = new PaymentTerm(nameof(CIF), "CIF");
     public static readonly PaymentTerm NetTerms = new PaymentTerm(nameof(NetTerms), "Net Terms 30day");
     public static readonly PaymentTerm CreditCard = new PaymentTerm(nameof(CreditCard), "Credit Card");
